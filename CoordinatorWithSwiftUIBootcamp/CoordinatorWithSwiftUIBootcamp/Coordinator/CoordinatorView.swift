3a8f0979c9b4261ac86af68f224535222045491e
Karambir <karmvirdahiya.com@gmail.com>
2026-06-08 22:30:02 +0530

    Co ordinator View Added.

---
+++ CoordinatorWithSwiftUIBootcamp/CoordinatorWithSwiftUIBootcamp/Coordinator/CoordinatorView.swift
@@ -0,0 +1,31 @@
+//
+//  CoordinatorView.swift
+//  CoordinatorWithSwiftUIBootcamp
+//
+//  Created by Siyaa Dahiya on 08/06/26.
+//
+
+import SwiftUI
+
+struct CoordinatorView: View {
+    @StateObject private var coordinator: Coordinator = Coordinator()
+    
+    var body: some View {
+        NavigationStack(path: $coordinator.path) {
+            coordinator.build(page: .apple)
+                .navigationDestination(for: Page.self) { page in
+                    coordinator.build(page: page)
+                }
+                .sheet(item: $coordinator.sheet) { sheet in
+                    coordinator.build(sheet: sheet)
+                }
+                .fullScreenCover(item: $coordinator.fullScreenCover) { fullScreenCover in
+                    coordinator.build(fullScreenCover: fullScreenCover)
+                }
+        }
+    }
+}
+
+#Preview {
+    CoordinatorView()
+}
