407dbd2655d9f965586c96f9eca993bab1547e76
Karambir <karmvirdahiya.com@gmail.com>
2026-06-08 22:22:09 +0530

    Coordinator class designed.

---
+++ CoordinatorWithSwiftUIBootcamp/CoordinatorWithSwiftUIBootcamp/Coordinator/Coordinator.swift
@@ -0,0 +1,100 @@
+//
+//  Coordinator.swift
+//  CoordinatorWithSwiftUIBootcamp
+//
+//  Created by Siyaa Dahiya on 08/06/26.
+//
+
+import Combine
+import SwiftUI
+
+enum Page: String, Identifiable {
+    case apple, banana, carrot
+    
+    var id: String {
+        self.rawValue
+    }
+}
+
+enum Sheet: String, Identifiable {
+    case lemon
+    
+    var id: String {
+        self.rawValue
+    }
+}
+
+enum FullScreenCover: String, Identifiable {
+    case olive
+    
+    var id: String {
+        self.rawValue
+    }
+}
+
+
+final class Coordinator: ObservableObject {
+    
+    @Published var path: NavigationPath = NavigationPath()
+    @Published var sheet: Sheet?
+    @Published var fullScreenCover: FullScreenCover?
+    
+    func push(_ page: Page) {
+        path.append(page)
+    }
+    
+    func present(_ sheet: Sheet) {
+        self.sheet = sheet
+    }
+    
+    func present(_ fullScreenCover: FullScreenCover) {
+        self.fullScreenCover = fullScreenCover
+    }
+    
+    func pop() {
+        path.removeLast()
+    }
+    
+    func popToRoot() {
+        path.removeLast(path.count)
+    }
+    
+    func dismissSheet() {
+        self.sheet = nil
+    }
+    
+    func dismissFullScreenCover() {
+        self.fullScreenCover = nil
+    }
+    
+    @ViewBuilder
+    func build(page: Page) -> some View {
+        switch page {
+        case .apple:
+            AppleView()
+        case .banana:
+            BananaView()
+        case .carrot:
+            CarrotView()
+        }
+    }
+    
+    @ViewBuilder
+    func build(sheet: Sheet) -> some View {
+        switch sheet {
+        case .lemon:
+            LemonView()
+        }
+    }
+    
+    @ViewBuilder
+    func build(fullScreenCover: FullScreenCover) -> some View {
+        switch fullScreenCover {
+        case .olive:
+            OliveView()
+        }
+    }
+    
+    
+}
+
