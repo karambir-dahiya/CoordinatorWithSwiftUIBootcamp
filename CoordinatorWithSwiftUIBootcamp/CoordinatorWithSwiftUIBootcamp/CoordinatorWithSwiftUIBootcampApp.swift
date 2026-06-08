83a70459b41ba7d78fa0b97cc6f4dcb8eab09d16
Karambir <karmvirdahiya.com@gmail.com>
2026-06-08 21:38:43 +0530

    initial Commit.

---
+++ CoordinatorWithSwiftUIBootcamp/CoordinatorWithSwiftUIBootcamp/CoordinatorWithSwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorWithSwiftUIBootcampApp.swift
+//  CoordinatorWithSwiftUIBootcamp
+//
+//  Created by Siyaa Dahiya on 08/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorWithSwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
